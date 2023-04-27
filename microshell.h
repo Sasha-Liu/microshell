1ce3323e786eba52dc1f1d6102a5dece08bef29a
Hsin-yu Liu <hsliu@z3r8p3.42lyon.fr>
2023-04-27 13:06:09 +0200

    first commit

---
+++ microshell.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   microshell.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsliu <hsliu@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/27 11:56:02 by hsliu             #+#    #+#             */
+/*   Updated: 2023/04/27 11:58:29 by hsliu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MICROSHELL_H
+# define MICROSHELL_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+
+
+
+
+#endif
