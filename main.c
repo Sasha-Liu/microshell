ca25718c4d15634e5b080b52686283824384156d
Hsin Yu Liu <dewseileaf@gmail.com>
2023-05-01 15:58:27 +0200

    wip

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sasha <sasha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 11:59:32 by hsliu             #+#    #+#             */
-/*   Updated: 2023/05/01 12:40:45 by sasha            ###   ########.fr       */
+/*   Updated: 2023/05/01 15:57:23 by sasha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 
 int	main(int argc, char **argv)
 {
+	int		i;
+	int		fd[4];
+	int		pid;
+	char	**args;
+	
 	if (argc == 1)
 	{
 		return (0);
@@ -22,7 +27,30 @@ int	main(int argc, char **argv)
 	{
 		exit(cd(argc - 1, argv + 1));
 	}
-	
+	i = 1;
+	//pipe(fd);
+	while (argv[i])
+	{
+		args = ft_get_args(i, argv);
+		if (!args)
+			exit(1);
+		pipe(fd + (i % 2) * 2);
+		pid = fork();
+		if (pid == 0)
+		{
+			ft_child_exec(i, argv, args, fd);
+		}
+		else
+		{
+			//wait for the child to terminate
+			//augument i 
+			//read from pipe
+			dup2(fd[0], 0);
+			//write to pipe or stdout
+			
+		}
+		i++;
+	}
 	
 	
 }
--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: sasha <sasha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 12:36:35 by sasha             #+#    #+#             */
-/*   Updated: 2023/05/01 12:44:24 by sasha            ###   ########.fr       */
+/*   Updated: 2023/05/01 15:44:23 by sasha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,4 +39,82 @@ int	cd(int argc, char **argv)
 		return (1);
 	}
 	return (0);
+}
+
+/* 
+	return a char ** for execve to use as argument
+*/
+char	**ft_get_args(int i, char **argv)
+{
+	char	**p;
+	int		count;
+
+	count = 0;
+	while (argv[i] && strcmp("|", argv[i]) && strcmp(";", argv[i]))
+	{
+		i++;
+		count++;
+	}
+	p = malloc(sizeof(char *) * (count + 1));
+	if (!p)
+	{
+		return (NULL);
+	}
+	p[count] = NULL;
+	while (count > 0)
+	{
+		count--;
+		i--;
+		p[count] = argv[i];
+	}
+	return (p);
+}
+
+void	ft_increment_i(int *i, char **argv)
+{
+	while (argv[*i] && strcmp("|", argv[*i]) && strcmp(";", argv[*i]))
+	{
+		*i++;
+	}
+	if (!(argv[*i]))
+		return ;
+	(*i)++;
+}
+
+void	ft_child_exec(int i, char **argv, char **args, int fd[2])
+{
+	extern char	**environ;
+	
+	//if is the first cmd or preceded by ;
+	if (i == 1 || !strcmp(";", argv[i - 1]))
+		;
+	else
+		dup2(fd[0], 0);
+	dup2(fd[1], 1);
+	execve(args[1], args, environ);
+	write(2, "error: cannot execute ", 22);
+	write(2, args[0], ft_strlen(args[0]));
+	write(2, "\n", 1);
+	exit(1);
+}
+
+/*
+	increment i, free resource
+*/
+void	ft_parent(int i, char **argv, char **args, int fd[2])
+{
+	int	status;
+	//int	temp[2];
+	
+	if (i == 1 || !strcmp(";", argv[i - 1]))
+		close(fd[0]);
+	
+	close(fd[1]);
+	waitpid(-1, &status, 0);
+	//pipe(temp);
+	//read from pipe (store where ?)
+	
+	//close read end
+	//reopen pipe
+	//write into pipe or stdout
 }
